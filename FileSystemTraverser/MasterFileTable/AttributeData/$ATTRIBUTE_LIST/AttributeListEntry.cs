b3c454396f3a14c8ebbfc3a11dbf852ca872242b
vellxalization <abcd1234abcd12346738t68374@gmail.com>
2025-02-23 01:18:34 +0300

    end of the day: added bitmap, tried to add attribute list, but it's generating gibberish rn

---
+++ FileSystemTraverser/MasterFileTable/AttributeData/$ATTRIBUTE_LIST/AttributeListEntry.cs
@@ -0,0 +1,29 @@
+﻿using FileSystemTraverser.MasterFileTable.AttributeRecord;
+using FileSystemTraverser.MasterFileTable.Header;
+
+namespace FileSystemTraverser.MasterFileTable.AttributeData._ATTRIBUTE_LIST;
+
+public record struct AttributeListEntry(AttributeType AttributeType, ushort Size, byte NameSize, byte NameOffset, 
+    ulong Vcn, MftSegmentReference FileReference, ushort AttributeId, byte[] Name)
+{
+    public static AttributeListEntry CreateFromStream(BinaryReader reader)
+    {
+        var start = reader.BaseStream.Position;
+        var attributeType = reader.ReadUInt32();
+        var size = reader.ReadUInt16();
+        var nameSize = reader.ReadByte();
+        var nameOffset = reader.ReadByte();
+        var vcn = reader.ReadUInt64();
+        var fileReference = MftSegmentReference.CreateFromStream(reader);
+        var attributeId = reader.ReadUInt16();
+        byte[] name = [];
+        if (nameSize != 0)
+        {
+            reader.BaseStream.Position = nameOffset;
+            name = reader.ReadBytes(nameSize * 2); // utf-16 encoded, 2 bytes per char
+        }
+
+        reader.BaseStream.Position = start + Math.Abs(reader.BaseStream.Length - size);
+        return new AttributeListEntry((AttributeType)attributeType, size, nameSize, nameOffset, vcn, fileReference, attributeId, name);
+    }
+}
---
+++ FileSystemTraverser/MasterFileTable/AttributeData/$BITMAP/Bitmap.cs
@@ -0,0 +1,17 @@
+﻿namespace FileSystemTraverser.MasterFileTable.AttributeData._BITMAP;
+
+public record struct Bitmap(byte[] Data)
+{
+    public static Bitmap CreateFromData(ref byte[] data)
+    {
+        return new Bitmap(data);
+    }
+
+    public bool IsSet(int bitIndex)
+    {
+        var byteIndex = bitIndex / 8;
+        var bitIndexInByte = bitIndex % 8;
+        var bitMask = 1 << bitIndexInByte; // read from the least significant bit
+        return (Data[byteIndex] & bitMask) != 0;
+    }
+}
