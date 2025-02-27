ee34ff6b109dee0e40bd9e794e9379088d457e15
vellxalization <abcd1234abcd12346738t68374@gmail.com>
2025-02-27 19:44:42 +0300

    fix for the "way too large attribute size" problem

--- NtfsParser/MasterFileTable/MftRecord.cs
+++ NtfsParser/MasterFileTable/MftRecord.cs
@@ -15,15 +15,58 @@ public record struct MftRecord(MftRecordHeader RecordHeader, MftAttribute[] Attr
         var attribute = MftAttribute.Parse(attributesSpan);
         while (attribute.Header.Type != AttributeType.EndOfAttributeList)
         {
+            int splitPoint = (int)attribute.Header.Size;
+            if (splitPoint > attributesSpan.Length)
+            {
+                splitPoint = GetAttributeRecordSize(attribute); 
+                // some attributes might have an inadequate size in the header so we need to calculate the proper size.
+                // no idea why this is happening
+                attribute = attribute with
+                {
+                    Header = attribute.Header with
+                    {
+                        Size = (uint)splitPoint
+                    }
+                };
+            }
+            
             attributes.Add(attribute);
-            var splitPoint = (int)attribute.Header.Size; 
+            attributesSpan = attributesSpan.Slice(splitPoint);
             // ^ using this to avoid headache of constantly caching ^
             // starting position of the reader
-            attributesSpan = attributesSpan.Slice(splitPoint);
             attribute = MftAttribute.Parse(attributesSpan);
         }
         
         // rest is unused bytes
         return new MftRecord(header, attributes.ToArray());
+    }
+
+    private static int GetAttributeRecordSize(MftAttribute attribute)
+    {
+        var header = attribute.Header;
+        int totalSize;
+        int rem;
+        if (!header.IsNonresident)
+        {
+            totalSize = header.Resident.Offset;
+            totalSize += (int)header.Resident.Size;
+            rem = totalSize % 8;
+            if (rem != 0)
+            {
+                totalSize += 8 - rem; // add 8-byte alignment padding
+            }
+            
+            return totalSize;
+        }
+
+        totalSize = header.Nonresident.DataRunsOffset;
+        totalSize += attribute.Value.Length + 1; // add 1 to compensate for the not included 0xFF header
+        rem = totalSize % 8;
+        if (rem != 0)
+        {
+            totalSize += 8 - rem; // add 8-byte alignment padding
+        }
+        
+        return totalSize;
     }
 }
